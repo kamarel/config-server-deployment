31da85afa04ae68284e037c0a5da48cdfd7bb972
kamarel <kamarelngunda@gmail.com>
2025-02-26 20:39:41 -0500

    last

---
+++ src/main/java/com/myKcc/com/config_server/WebConfig.java
@@ -0,0 +1,20 @@
+package com.myKcc.com.config_server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
+
